9a8b9ff963b1119225eb3fbd997014c7394eaee9
bartalshir <bartalshir@gmail.com>
2018-06-25 14:44:50 +0300

    hw

--- shir/Bartal_s/bartalsjs.js
+++ shir/Bartal_s/bartalsjs.js
@@ -2,3 +2,17 @@
     document.getElementById("mainpage").style.display="none";
     document.getElementById("contactus").style.display="block";
   }
+  $(function(){
+    $("#drinksPhoto").click(function(){
+      $("#pDrinks").slideToggle();
+    });
+    $("#foodPhoto").click(function(){
+      $("#pFood").slideToggle();
+    });
+    $("#specialsPhoto").click(function(){
+      $("#pSpecials").slideToggle();
+    });
+    $("#desertPhoto").click(function(){
+      $("#pDeserts").slideToggle();
+    });
+  });
